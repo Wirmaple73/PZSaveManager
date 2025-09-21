cd0e13a964dd3d62143c0c213da9205b88340a57
Wirmaple73 <wirmaple73@gmail.com>
2025-09-21 16:09:45 +0330

    Add options for reporting issues and sending feedback.

--- PZSaveManager/Forms/MainForm.Designer.cs
+++ PZSaveManager/Forms/MainForm.Designer.cs
@@ -20,135 +20,163 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			menuStrip = new MenuStrip();
-			fileToolStripMenuItem = new ToolStripMenuItem();
-			openLogFileToolStripMenuItem = new ToolStripMenuItem();
-			toolStripMenuItem1 = new ToolStripSeparator();
-			exitToolStripMenuItem = new ToolStripMenuItem();
-			optionsToolStripMenuItem = new ToolStripMenuItem();
-			configureSaveOptionsToolStripMenuItem = new ToolStripMenuItem();
-			helpToolStripMenuItem = new ToolStripMenuItem();
-			checkForUpdatesToolStripMenuItem = new ToolStripMenuItem();
-			aboutToolStripMenuItem = new ToolStripMenuItem();
-			pagePanel = new Panel();
-			menuStrip.SuspendLayout();
-			SuspendLayout();
-			// 
-			// menuStrip
-			// 
-			menuStrip.AllowMerge = false;
-			menuStrip.BackColor = SystemColors.Menu;
-			menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, optionsToolStripMenuItem, helpToolStripMenuItem });
-			menuStrip.Location = new Point(0, 0);
-			menuStrip.Name = "menuStrip";
-			menuStrip.Size = new Size(784, 24);
-			menuStrip.TabIndex = 0;
-			menuStrip.Text = "menuStrip";
-			// 
-			// fileToolStripMenuItem
-			// 
-			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openLogFileToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
-			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			fileToolStripMenuItem.Size = new Size(37, 20);
-			fileToolStripMenuItem.Text = "&File";
-			// 
-			// openLogFileToolStripMenuItem
-			// 
-			openLogFileToolStripMenuItem.Name = "openLogFileToolStripMenuItem";
-			openLogFileToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.L;
-			openLogFileToolStripMenuItem.Size = new Size(196, 22);
-			openLogFileToolStripMenuItem.Text = "&Open Log File...";
-			openLogFileToolStripMenuItem.Click += openLogFileToolStripMenuItem_Click;
-			// 
-			// toolStripMenuItem1
-			// 
-			toolStripMenuItem1.Name = "toolStripMenuItem1";
-			toolStripMenuItem1.Size = new Size(193, 6);
-			// 
-			// exitToolStripMenuItem
-			// 
-			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
-			exitToolStripMenuItem.Size = new Size(196, 22);
-			exitToolStripMenuItem.Text = "&Exit";
-			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
-			// 
-			// optionsToolStripMenuItem
-			// 
-			optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { configureSaveOptionsToolStripMenuItem });
-			optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-			optionsToolStripMenuItem.Size = new Size(61, 20);
-			optionsToolStripMenuItem.Text = "&Options";
-			// 
-			// configureSaveOptionsToolStripMenuItem
-			// 
-			configureSaveOptionsToolStripMenuItem.Name = "configureSaveOptionsToolStripMenuItem";
-			configureSaveOptionsToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.E;
-			configureSaveOptionsToolStripMenuItem.Size = new Size(248, 22);
-			configureSaveOptionsToolStripMenuItem.Text = "&Configure Save Options...";
-			configureSaveOptionsToolStripMenuItem.Click += configureSaveOptionsToolStripMenuItem_Click;
-			// 
-			// helpToolStripMenuItem
-			// 
-			helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { checkForUpdatesToolStripMenuItem, aboutToolStripMenuItem });
-			helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-			helpToolStripMenuItem.Size = new Size(44, 20);
-			helpToolStripMenuItem.Text = "&Help";
-			// 
-			// checkForUpdatesToolStripMenuItem
-			// 
-			checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
-			checkForUpdatesToolStripMenuItem.Size = new Size(180, 22);
-			checkForUpdatesToolStripMenuItem.Text = "Check for &Updates...";
-			checkForUpdatesToolStripMenuItem.Click += checkForUpdatesToolStripMenuItem_Click;
-			// 
-			// aboutToolStripMenuItem
-			// 
-			aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-			aboutToolStripMenuItem.Size = new Size(180, 22);
-			aboutToolStripMenuItem.Text = "&About";
-			aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
-			// 
-			// pagePanel
-			// 
-			pagePanel.Dock = DockStyle.Fill;
-			pagePanel.Location = new Point(0, 24);
-			pagePanel.Name = "pagePanel";
-			pagePanel.Size = new Size(784, 537);
-			pagePanel.TabIndex = 1;
-			// 
-			// MainForm
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			BackColor = SystemColors.Window;
-			ClientSize = new Size(784, 561);
-			Controls.Add(pagePanel);
-			Controls.Add(menuStrip);
-			Icon = Properties.Resources.Icon;
-			KeyPreview = true;
-			MainMenuStrip = menuStrip;
-			MinimumSize = new Size(540, 350);
-			Name = "MainForm";
-			Text = "Project Zomboid Save Manager";
-			FormClosing += MainForm_FormClosing;
-			Shown += MainForm_Shown;
-			menuStrip.ResumeLayout(false);
-			menuStrip.PerformLayout();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openLogFileToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            optionsToolStripMenuItem = new ToolStripMenuItem();
+            configureSaveOptionsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            checkForUpdatesToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripSeparator();
+            sendFeedbackToolStripMenuItem = new ToolStripMenuItem();
+            reportToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            pagePanel = new Panel();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            menuStrip.AllowMerge = false;
+            menuStrip.BackColor = SystemColors.Menu;
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, optionsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(784, 24);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openLogFileToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // openLogFileToolStripMenuItem
+            // 
+            openLogFileToolStripMenuItem.Name = "openLogFileToolStripMenuItem";
+            openLogFileToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.L;
+            openLogFileToolStripMenuItem.Size = new Size(196, 22);
+            openLogFileToolStripMenuItem.Text = "&Open Log File...";
+            openLogFileToolStripMenuItem.Click += openLogFileToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(193, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            exitToolStripMenuItem.Size = new Size(196, 22);
+            exitToolStripMenuItem.Text = "&Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // optionsToolStripMenuItem
+            // 
+            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { configureSaveOptionsToolStripMenuItem });
+            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            optionsToolStripMenuItem.Size = new Size(61, 20);
+            optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // configureSaveOptionsToolStripMenuItem
+            // 
+            configureSaveOptionsToolStripMenuItem.Name = "configureSaveOptionsToolStripMenuItem";
+            configureSaveOptionsToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.E;
+            configureSaveOptionsToolStripMenuItem.Size = new Size(248, 22);
+            configureSaveOptionsToolStripMenuItem.Text = "&Configure Save Options...";
+            configureSaveOptionsToolStripMenuItem.Click += configureSaveOptionsToolStripMenuItem_Click;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { checkForUpdatesToolStripMenuItem, toolStripMenuItem3, sendFeedbackToolStripMenuItem, reportToolStripMenuItem, toolStripMenuItem2, aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "&Help";
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            checkForUpdatesToolStripMenuItem.Size = new Size(228, 22);
+            checkForUpdatesToolStripMenuItem.Text = "Check for &Updates...";
+            checkForUpdatesToolStripMenuItem.Click += checkForUpdatesToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(225, 6);
+            // 
+            // sendFeedbackToolStripMenuItem
+            // 
+            sendFeedbackToolStripMenuItem.Name = "sendFeedbackToolStripMenuItem";
+            sendFeedbackToolStripMenuItem.Size = new Size(228, 22);
+            sendFeedbackToolStripMenuItem.Text = "&Send Feedback";
+            sendFeedbackToolStripMenuItem.Click += sendFeedbackToolStripMenuItem_Click;
+            // 
+            // reportToolStripMenuItem
+            // 
+            reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            reportToolStripMenuItem.Size = new Size(228, 22);
+            reportToolStripMenuItem.Text = "&Feature Request / Bug Report";
+            reportToolStripMenuItem.Click += reportToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(225, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(228, 22);
+            aboutToolStripMenuItem.Text = "&About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // pagePanel
+            // 
+            pagePanel.Dock = DockStyle.Fill;
+            pagePanel.Location = new Point(0, 24);
+            pagePanel.Name = "pagePanel";
+            pagePanel.Size = new Size(784, 537);
+            pagePanel.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(784, 561);
+            Controls.Add(pagePanel);
+            Controls.Add(menuStrip);
+            Icon = Properties.Resources.Icon;
+            KeyPreview = true;
+            MainMenuStrip = menuStrip;
+            MinimumSize = new Size(540, 350);
+            Name = "MainForm";
+            Text = "Project Zomboid Save Manager";
+            FormClosing += MainForm_FormClosing;
+            Shown += MainForm_Shown;
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-		private MenuStrip menuStrip;
+        #endregion
+        private MenuStrip menuStrip;
 		private ToolStripMenuItem fileToolStripMenuItem;
 		private ToolStripMenuItem exitToolStripMenuItem;
 		private ToolStripMenuItem optionsToolStripMenuItem;
@@ -159,5 +187,9 @@
 		private Panel pagePanel;
 		private ToolStripMenuItem openLogFileToolStripMenuItem;
 		private ToolStripSeparator toolStripMenuItem1;
-	}
+        private ToolStripMenuItem sendFeedbackToolStripMenuItem;
+        private ToolStripMenuItem reportToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem3;
+        private ToolStripSeparator toolStripMenuItem2;
+    }
 }
--- PZSaveManager/Forms/MainForm.cs
+++ PZSaveManager/Forms/MainForm.cs
@@ -4,149 +4,152 @@ using PZSaveManager.Properties;
 
 namespace PZSaveManager.Forms
 {
-	public partial class MainForm : Form
-	{
-		private readonly WorldSelectionPage worldSelectionPage = new();
-		private readonly SaveSelectionPage saveSelectionPage = new();
+    public partial class MainForm : Form
+    {
+        private readonly WorldSelectionPage worldSelectionPage = new();
+        private readonly SaveSelectionPage saveSelectionPage = new();
 
-		public MainForm() => InitializeComponent();
+        public MainForm() => InitializeComponent();
 
-		private void MainForm_Shown(object sender, EventArgs e)
-		{
-			InitializeApplication();
-			CheckForInsufficientDiskSpace();
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            InitializeApplication();
+            CheckForInsufficientDiskSpace();
 
-			SaveHelper.UpdateAutosaveTimer();
+            SaveHelper.UpdateAutosaveTimer();
 
-			if (!SaveHelper.Hotkeys.UpdateAll() && MessageBoxManager.ShowConfirmation("One of the save hotkeys could not be loaded properly. Would you like to open the save options now?", "Save Hotkey Error", isYesDefault: true))
-				configureSaveOptionsToolStripMenuItem_Click(this, EventArgs.Empty);
+            if (!SaveHelper.Hotkeys.UpdateAll() && MessageBoxManager.ShowConfirmation("One of the save hotkeys could not be loaded properly. Would you like to open the save options now?", "Save Hotkey Error", isYesDefault: true))
+                configureSaveOptionsToolStripMenuItem_Click(this, EventArgs.Empty);
 
 
-			void InitializeApplication()
-			{
-				Application.ApplicationExit += Application_ApplicationExit;
+            void InitializeApplication()
+            {
+                Application.ApplicationExit += Application_ApplicationExit;
 
-				worldSelectionPage.NextButton.Click += NextButton_Click;
-				saveSelectionPage.BackButton.Click += BackButton_Click;
+                worldSelectionPage.NextButton.Click += NextButton_Click;
+                saveSelectionPage.BackButton.Click += BackButton_Click;
 
-				this.AcceptButton = worldSelectionPage.NextButton;
-				BackButton_Click(this, EventArgs.Empty);
-			}
+                this.AcceptButton = worldSelectionPage.NextButton;
+                BackButton_Click(this, EventArgs.Empty);
+            }
 
-			void CheckForInsufficientDiskSpace()
-			{
-				const double LowDiskSpaceThreshold = 3;  // in gigabytes
-				double freeSpace = new DriveInfo(Save.BackupPath).AvailableFreeSpace / 1e+9;  // in gigabytes
+            void CheckForInsufficientDiskSpace()
+            {
+                const double LowDiskSpaceThreshold = 3;  // in gigabytes
+                double freeSpace = new DriveInfo(Save.BackupPath).AvailableFreeSpace / 1e+9;  // in gigabytes
 
-				if (freeSpace < LowDiskSpaceThreshold && MessageBoxManager.ShowConfirmation($"You are currently low on disk space (<{LowDiskSpaceThreshold} GB). This may cause newer saves to completely fill up your disk space. You are suggested to change the save backup path to another drive.\n\nWould you like to do that now?", "Low Disk Space", isYesDefault: true))
-					configureSaveOptionsToolStripMenuItem_Click(this, EventArgs.Empty);
-			}
-		}
+                if (freeSpace < LowDiskSpaceThreshold && MessageBoxManager.ShowConfirmation($"You are currently low on disk space (<{LowDiskSpaceThreshold} GB). This may cause newer saves to completely fill up your disk space. You are suggested to change the save backup path to another drive.\n\nWould you like to do that now?", "Low Disk Space", isYesDefault: true))
+                    configureSaveOptionsToolStripMenuItem_Click(this, EventArgs.Empty);
+            }
+        }
 
-		private void BackButton_Click(object? sender, EventArgs e)
-		{
-			PageLoader.Load(pagePanel, worldSelectionPage);
+        private void BackButton_Click(object? sender, EventArgs e)
+        {
+            PageLoader.Load(pagePanel, worldSelectionPage);
 
-			this.CancelButton = saveSelectionPage.BackButton;
-			this.ContextMenuStrip = worldSelectionPage.ContextMenuStrip;
-		}
+            this.CancelButton = saveSelectionPage.BackButton;
+            this.ContextMenuStrip = worldSelectionPage.ContextMenuStrip;
+        }
 
-		private void NextButton_Click(object? sender, EventArgs e)
-		{
-			saveSelectionPage.SelectedWorld = worldSelectionPage.SelectedWorld;
-			PageLoader.Load(pagePanel, saveSelectionPage);
+        private void NextButton_Click(object? sender, EventArgs e)
+        {
+            saveSelectionPage.SelectedWorld = worldSelectionPage.SelectedWorld;
+            PageLoader.Load(pagePanel, saveSelectionPage);
 
-			this.ContextMenuStrip = saveSelectionPage.ContextMenuStrip;
-		}
+            this.ContextMenuStrip = saveSelectionPage.ContextMenuStrip;
+        }
 
-		private void configureSaveOptionsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			using var form = new SaveOptionsForm();
+        private void configureSaveOptionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using var form = new SaveOptionsForm();
 
-			if (form.ShowDialog() == DialogResult.OK)
-			{
-				SaveHelper.UpdateAutosaveTimer();
-				saveSelectionPage.UpdateUI();
-			}
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                SaveHelper.UpdateAutosaveTimer();
+                saveSelectionPage.UpdateUI();
+            }
 
-			SaveHelper.Hotkeys.UpdateAll();
-		}
+            SaveHelper.Hotkeys.UpdateAll();
+        }
 
-		private void openLogFileToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			if (!FileExplorer.Browse(Logger.FilePath))
-				MessageBoxManager.ShowError($"The log file could not be opened. It may still be opened manually at {Logger.FilePath}.");
-		}
+        private void openLogFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!FileExplorer.Browse(Logger.FilePath))
+                MessageBoxManager.ShowError($"The log file could not be opened. It may still be opened manually at {Logger.FilePath}.");
+        }
 
-		private void exitToolStripMenuItem_Click(object sender, EventArgs e) => this.Close();
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) => this.Close();
 
-		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			using var form = new AboutForm();
-			form.ShowDialog();
-		}
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using var form = new AboutForm();
+            form.ShowDialog();
+        }
 
-		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			if (SaveHelper.Hotkeys.GetKeyByString(Settings.Default.SaveHotkey).Key is null && !Settings.Default.EnableAutosave)
-				return;
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (SaveHelper.Hotkeys.GetKeyByString(Settings.Default.SaveHotkey).Key is null && !Settings.Default.EnableAutosave)
+                return;
 
-			if (!MessageBoxManager.ShowConfirmation("Are you sure you want to quit? The program must be kept running in the background in order to perform manual or automatic saves!", "Exit Confirmation"))
-				e.Cancel = true;
-		}
+            if (!MessageBoxManager.ShowConfirmation("Are you sure you want to quit? The program must be kept running in the background in order to perform manual or automatic saves!", "Exit Confirmation"))
+                e.Cancel = true;
+        }
 
-		private void Application_ApplicationExit(object? sender, EventArgs e)
-		{
-			SaveHelper.Hotkeys.UnbindAll();
+        private void Application_ApplicationExit(object? sender, EventArgs e)
+        {
+            SaveHelper.Hotkeys.UnbindAll();
 
-			Settings.Default.Save();
-			Logger.Log("All settings have been saved.", LogSeverity.Info);
+            Settings.Default.Save();
+            Logger.Log("All settings have been saved.", LogSeverity.Info);
 
-			SoundPlayer.Shared.Dispose();
-			SaveHelper.Dispose();
+            SoundPlayer.Shared.Dispose();
+            SaveHelper.Dispose();
 
-			Logger.Log("Application shutting down.", LogSeverity.Info);
-			Logger.Dispose();
-		}
+            Logger.Log("Application shutting down.", LogSeverity.Info);
+            Logger.Dispose();
+        }
 
-		private async void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			Version newVersion;
+        private async void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Version newVersion;
 
-			try
-			{
-				newVersion = await VersionManager.GetLatestVersion();
-			}
-			catch (Exception ex)
-			{
-				Logger.Log("Could not check for updates", ex);
-				MessageBoxManager.ShowError($"Could not check for updates. Please ensure you are properly connected to the internet and try again.\n\nError message: {ex.Message}");
-				return;
-			}
+            try
+            {
+                newVersion = await VersionManager.GetLatestVersion();
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Could not check for updates", ex);
+                MessageBoxManager.ShowError($"Could not check for updates. Please ensure you are properly connected to the internet and try again.\n\nError message: {ex.Message}");
+                return;
+            }
 
-			if (newVersion > VersionManager.CurrentVersion)
-			{
-				if (MessageBoxManager.ShowConfirmation("A new version is available. Would you like to open the download page now?", "Update Confirmation", MessageBoxIcon.Asterisk, true))
-					FileExplorer.Browse(VersionManager.RepoUrl);
-			}
-			else
-			{
-				MessageBoxManager.ShowInfo("You are currently running the latest version of the program.", "Update Check");
-			}
-		}
+            if (newVersion > VersionManager.CurrentVersion)
+            {
+                if (MessageBoxManager.ShowConfirmation("A new version is available. Would you like to open the download page now?", "Update Confirmation", MessageBoxIcon.Asterisk, true))
+                    FileExplorer.Browse(VersionManager.RepoUrl);
+            }
+            else
+            {
+                MessageBoxManager.ShowInfo("You are currently running the latest version of the program.", "Update Check");
+            }
+        }
 
-		// TODO: Figure out a way to prevent controls from stealing pressed keys
-		//protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-		//{
-		//	if (PageLoader.CurrentPage is not null)
-		//	{
-		//		// Propagate pressed keys to child pages
-		//		((IPage)PageLoader.CurrentPage).GlobalKeyDown(keyData);
+        private void sendFeedbackToolStripMenuItem_Click(object sender, EventArgs e) => FileExplorer.Browse(VersionManager.FeedbackUrl);
+        private void reportToolStripMenuItem_Click(object sender, EventArgs e) => FileExplorer.Browse(VersionManager.IssueReportUrl);
 
-		//		return true;
-		//	}
+        // TODO: Figure out a way to prevent controls from stealing pressed keys
+        //protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        //{
+        //	if (PageLoader.CurrentPage is not null)
+        //	{
+        //		// Propagate pressed keys to child pages
+        //		((IPage)PageLoader.CurrentPage).GlobalKeyDown(keyData);
 
-		//	return base.ProcessCmdKey(ref msg, keyData);
-		//}
-	}
+        //		return true;
+        //	}
+
+        //	return base.ProcessCmdKey(ref msg, keyData);
+        //}
+    }
 }
