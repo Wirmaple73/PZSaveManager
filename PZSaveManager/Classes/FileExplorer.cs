574d7a491961868fcbd9f78c263398ebdcb98a2a
Wirmaple73 <wirmaple73@gmail.com>
2025-09-25 13:15:25 +0330

    Add an alternative overload for selecting files/directories.

--- PZSaveManager/Classes/FileExplorer.cs
+++ PZSaveManager/Classes/FileExplorer.cs
@@ -1,22 +1,29 @@
-﻿using System.Diagnostics;
+﻿using SharpCompress.Common;
+using System.Diagnostics;
 
 namespace PZSaveManager.Classes
 {
 	public static class FileExplorer
 	{
-		public static bool Browse(string path)
-		{
-			try
-			{
-				Process.Start("explorer.exe", path);
-			}
-			catch (Exception ex)
-			{
-				Logger.Log($"Could not browse {path}", ex);
-				return false;
-			}
+        public static bool Browse(string path) => InternalBrowse(path, false);
+        public static bool Browse(string path, bool highlight) => InternalBrowse(path, highlight);
 
-			return true;
-		}
+		private static bool InternalBrowse(string path, bool highlight)
+		{
+            try
+            {
+                if (highlight)
+                    Process.Start("explorer.exe", $"/select,\"{path}\"");
+                else
+                    Process.Start("explorer.exe", path);
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Could not browse {path}", ex);
+                return false;
+            }
+
+            return true;
+        }
 	}
 }
