86d77c0859de45699466ac917596cd5338069997
Wirmaple73 <wirmaple73@gmail.com>
2025-10-30 09:53:26 +0330

    Remove unused sound effects.

--- PZSaveManager/Classes/SoundEffect.cs
+++ PZSaveManager/Classes/SoundEffect.cs
@@ -2,7 +2,6 @@
 {
 	public enum SoundEffect
 	{
-		Saving, AlreadySaving, SaveCanceled, SaveNotCanceled, SaveComplete, SaveFailure,
-		Spam1, Spam2, Spam3, Spam4, Spam5, Spam6
+		Saving, AlreadySaving, SaveCanceled, SaveNotCanceled, SaveComplete, SaveFailure
 	}
 }
